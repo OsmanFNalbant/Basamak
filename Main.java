285204f008882cf562c3cddfc31ceaacc0d86f97
OsmanFurkanNalbant <osmanfnalbant@outlook.com>
2022-09-08 04:11:44 +0300

    BasamakHesabi

--- Main.java
+++ Main.java
@@ -7,11 +7,14 @@ public class Main {
     System.out.println("C(n,r) n'i giriniz:");
     int a=sc.nextInt();
     int x =1;
+    int toplam=0;
     while(a/10!=0) {
         System.out.println(x+ "'ler basamağı "+a%10);
+        toplam +=(a%10);
         a=a/10;
         x*=10;
     }
-    System.out.println((x*10)+"'ler basamağı"+a);
+    System.out.println((x*10)+"'ler basamağı "+a);
+    System.out.println("Basamak toplamı:"+(a+toplam));
     }
 }
