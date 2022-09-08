db023f12b126630707c44c910cb583dfeb1bfb5d
OsmanFurkanNalbant <osmanfnalbant@outlook.com>
2022-09-08 04:07:36 +0300

    BasamakHesabi

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("C(n,r) n'i giriniz:");
+    int a=sc.nextInt();
+    int x =1;
+    while(a/10!=0) {
+        System.out.println(x+ "'ler basamağı "+a%10);
+        a=a/10;
+        x*=10;
+    }
+    System.out.println((x*10)+"'ler basamağı"+a);
+    }
+}
